a4fe3d239113035c5050d63c23e54cbdbae6474d
Daneev <30390132+Daneev@users.noreply.github.com>
2017-07-25 19:10:44 +0400

    Создан класс групп

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
@@ -8,30 +9,11 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_page(self):
         wd = self.wd
         wd.get("http://addressbook/")
-
-    def open_groups(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
 
     def destroy(self):
         self.wd.quit()
