392feb068a8a9053a9412a08bb212f4c8f7ecdf5
Daneev <30390132+Daneev@users.noreply.github.com>
2017-07-26 20:07:18 +0400

    Добавлены тесты по модификации групп

--- fixture/application.py
+++ fixture/application.py
@@ -7,9 +7,16 @@ from fixture.session import SessionHelper
 class Application:
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
 
     def open_page(self):
         wd = self.wd
